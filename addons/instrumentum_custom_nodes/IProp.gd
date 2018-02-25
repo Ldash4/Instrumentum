487c5665d3a95c1dba39d21281af7698aeabc863
LfourZ <lfourunderscore@gmail.com>
2018-02-25 19:14:13 +0100

    Stopped interpolating rotation

--- addons/instrumentum_custom_nodes/IProp.gd
+++ addons/instrumentum_custom_nodes/IProp.gd
@@ -14,7 +14,7 @@ var prop_name
 slave var slave_translation = Vector3()
 #slave var slave_linear_velocity = Vector3()
 # Testing not using velocity interpolation
-slave var slave_angular_velocity = Vector3()
+#slave var slave_angular_velocity = Vector3()
 slave var slave_rotation = Vector3()
 
 # Glue
@@ -29,8 +29,8 @@ func _update_prop():
 		else:
 			translation = translation.linear_interpolate(slave_translation, INTERP_RATE)
 #			linear_velocity = slave_linear_velocity # see slave var declaration
-			angular_velocity = slave_angular_velocity
-			rotation = rotation.linear_interpolate(slave_rotation, INTERP_RATE)
+#			angular_velocity = slave_angular_velocity
+			rotation = slave_rotation
 
 func _ready():
 	Gamestate.connect("prop_tick", self, "_update_prop")
